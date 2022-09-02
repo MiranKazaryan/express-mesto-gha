ce750bf061d231f7dfa94c8b3d294a5113bd30fb
Miran Kazaryan <mir12344@mail.ru>
2022-09-02 11:38:10 +0300

    validation and middleware added

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,25 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const userRouter = require('./users');
 const cardRouter = require('./cards');
 const auth = require('../middlewares/auth');
 const { createUser, login } = require('../controllers/users');
 
-router.post('/signup', createUser);
-router.post('/signin', login);
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().allow('').min(2).max(30),
+    about: Joi.string().allow('').min(2).max(30),
+    avatar: Joi.string().pattern(/(http(s)?:\/\/)?(www\.)?[A-Za-zА-Яа-я0-9-]*\.[A-Za-zА-Яа-я0-9-]{2,8}(\/?[\wа-яА-Я#!:.?+=&%@!_~[\]$'*+,;=()-]*)*/),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  })
+}), login);
 
 router.use(auth);
 
